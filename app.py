a8830f0f1da230f1c65b5436ffd041a45b2c780a
Jon Thorne <jon@jonthorne.com>
2024-11-16 23:49:02 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import sys
+
+total_score = 0
+frame       = 1
+
+'''
+Dice breakdown: 
+3 - Strike  (3 pins, 2 blank, 1 strike)
+3 - Spare   (3 pins, 2 blank, 1 spare)
+4 - regular (3 pins, 3 blank)
+'''
+# probably want to create a game class which holds all 10 frames
+# a hand class which holds teh current "hand" of dice
+# and a dice class.
+# scoring will likely be the biggest pain in the ass
+
+def roll(frame=1,roll=1):
+    if roll == 1:
+        pass
+
+if __name__ == "__main__":
+    print("Coming soon")
+    sys.exit()
