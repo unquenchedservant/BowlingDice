5f637b1a7be318dd782172e1c06088f6d0bbb55b
Jon Thorne <jon@jonthorne.com>
2024-11-17 12:59:39 -0500

    Pretty much finalized the logic on the Dice class

--- dice.py
+++ dice.py
@@ -1,18 +1,36 @@
 import random
 class Dice:
     def __init__(self, type):
-        self.type = type
-        self.active = True
-        self.result = "standing"
+        self.type = type # 3 types, regular, strike, spare
+        self.active = True # if dice is active or not (strike, spare, down)
+        self.result = "standing" # 4 results, standing, down, strike, spare
 
-    def roll(self):
+    def roll(self, framePos=1):
         # get random number between 1 and 6
         result=random.randint(1,6)
-        if result == 6:
+    
+        if result == 1 or result == 2 or result == 3:
+            self.result = "standing"
+        elif result == 4 or result == 5:
+            self.result = "down"
+            self.active = False
+        elif result == 6:
             if self.type == "regular":
                 self.result = "down"
+                self.active = False
             elif self.type == "strike":
-                self.result = "strike"
+                if framePos == 1:
+                    self.result = "strike"
+                else:
+                    self.result = "down"
+                self.active = False
             elif self.type == "spare":
-                self.result = "spare"
+                if framePos == 1:
+                    self.result = "down"
+                else:
+                    self.result = "spare"
+                self.active = False
+        
     
+    def get_result(self):
+        return self.result
